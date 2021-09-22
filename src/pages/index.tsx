33cec513a9928149334a59cfc09bea57cdd075f1
Cleiton <cleiton.freitas1@gmail.com>
2021-09-22 14:59:41 -0400

    implementado componentes e paginas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,29 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import style from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello Word</h1>
+      <main className={style.contentContainer}>
+        <section className={style.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
