11c62fd47d2f136d71f277833edaecc7ea20977a
Cleiton <cleiton.freitas1@gmail.com>
2021-09-25 17:03:48 -0400

    Paginas estaticas

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
+import { ActiveLInk } from '../ActiveLink';
 
 export function Header() {
   return (
@@ -8,8 +9,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLInk activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLInk>
+          <ActiveLInk activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLInk>
         </nav>
         <SignInButton />
       </div>
