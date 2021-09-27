1d7b3ca8dc98472ab38754393b181254cdddd2a7
Cleiton <cleiton.freitas1@gmail.com>
2021-09-27 10:00:10 -0400

    Finalizando a aplicação

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -15,7 +15,7 @@ interface PostProps {
   };
 }
 
-export default function Post({ post }) {
+export default function Post({ post }: PostProps) {
   return (
     <>
       <Head>
@@ -42,8 +42,14 @@ export const getServerSideProps: GetServerSideProps = async ({
   const session = await getSession({ req });
   const { slug } = params;
 
-  //   if(!session) {
-  //   }
+  if (!session?.activeSubscription) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
 
   const prismic = getPrismicClient(req);
 
